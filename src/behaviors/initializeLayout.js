930f60f2e42333e57f04c05a1ab4841d186f0f02
Rafał Mikołajun <root@rmweb.pl>
2021-11-20 11:55:07 +0100

    app layout

--- src/behaviors/initializeLayout.js
+++ src/behaviors/initializeLayout.js
@@ -1,28 +1,26 @@
-import layoutReady from "./layoutReady";
-// import DrawerToggle from "./layout/DrawerToggle";
-// import PageProgress from "./layout/PageProgress";
-// import FormBehavior from "./FormBehavior";
+import layoutReady from './layoutReady.js';
+import DrawerToggle from './layout/DrawerToggle.js';
+import AppProgress from './layout/AppProgress.js';
+import FormBehavior from './FormBehavior.js';
 
 export default () => {
     layoutReady(() => {
-        // const progress = document.querySelector('#page-progress');
+        const progress = document.querySelector('#app-progress');
 
-        // if (progress !== null) {
-            // new PageProgress(progress);
-        // }
+        if (progress !== null) {
+            new AppProgress(progress);
+        }
 
         for (const el of document.querySelectorAll('*[wc-hidden], *[wc-lazy], *[wc-ready]')) {
             el.classList.add('ready');
         }
 
-        // for (const el of document.querySelectorAll('.drawer-toggle')) {
-            // new DrawerToggle(el);
-        // }
+        for (const el of document.querySelectorAll('.app-drawer-toggle')) {
+            new DrawerToggle(el);
+        }
 
-        // for (const el of document.querySelectorAll('.form-behavior')) {
-            // new FormBehavior(el);
-        // }
+        for (const el of document.querySelectorAll('.form-behavior')) {
+            new FormBehavior(el);
+        }
     });
 };
-
-
--- src/components/bundles/components.js
+++ src/components/bundles/components.js
@@ -1,2 +1,8 @@
-// elements
+// external
+import '@material/mwc-icon/mwc-icon.js';
+import '@material/mwc-icon-button/mwc-icon-button.js';
+import '@material/mwc-drawer/mwc-drawer.js';
+import '@material/mwc-linear-progress/mwc-linear-progress.js';
+
+// internal
 import '../elements/my-element/my-element.js';
