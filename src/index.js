744fedfd4a8222e841fe73551e23895dab1a83da
Rafał Mikołajun <root@rmweb.pl>
2021-11-20 02:06:37 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 // components bundles
 import './components/bundles/components.js';
+
+// app imports
+import namespace from './core/namespace.js';
+import initializeLayout from './behaviors/initializeLayout.js';
+
+initializeLayout();
+
+window.App = {
+    name: 'Lit-Boilerplate',
+    // export to global scope
+};
+
+namespace(window.App);
