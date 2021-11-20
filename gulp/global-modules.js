f9d86dadd0bb04f6d9dbfe4d56833b76a40b604b
Rafał Mikołajun <root@rmweb.pl>
2021-11-20 01:13:46 +0100

    gulp-global-modules - es modules fix

--- gulp/global-modules.js
+++ gulp/global-modules.js
@@ -1,10 +1,20 @@
-import fs from 'fs-extra';
+import {default as fsExtra} from 'fs-extra';
+import fs from 'fs';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const importJSON = (filename) => {
+    return JSON.parse(fs.readFileSync(filename));
+}
 
 const getGlobals = () => {
-    if (fs.pathExistsSync(__dirname + '/../globals.custom.json')) {
-        return require('../globals.custom.json');
+    if (fsExtra.pathExistsSync(__dirname + '/../globals.custom.json')) {
+        return importJSON(__dirname + '/../globals.custom.json');
     } else {
-        return require('../globals.default.json');
+        return importJSON(__dirname + '/../globals.default.json');
     }
 };
 
